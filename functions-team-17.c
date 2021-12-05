b5e7d4afe8da8c6eb0178b0f9f2fbe1a1ff4f8e3
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-05 20:47:11 +0000

    Update functions-team-17.c
    o tipo das funções foi ajustado e adequado ao pedido

--- functions-team-17.c
+++ functions-team-17.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-int formarVectorDim1(int a[], int x){ //esta função forma um vetor de 1 dimensão
+void formarVectorDim1(int a[], int x){ //esta função forma um vetor de 1 dimensão
     for (int i=0; i<x; i++){
-        printf("Indique o valor da %dª entrada:\n", i);
+        printf("Indique o valor da %dª entrada:\n", i+1);
         scanf("%d", &a[i]);
+        if (a[i]>=8 && a[i]<=27){
+            continue;
+        }
+        else {
+            while(a[i]<8 || a[i]>27){
+            printf("O valor não está compreendido entre 8 e 27!\n");
+            printf("Indique o valor da %dª entrada:\n", i+1);
+            scanf("%d", &a[i]);
+            }
+
+        }
     }
 }
-int formarVectorDim2(int y, int x, int a[][y]){ //esta função forma um vetor de 2 dimensões
+void formarVectorDim2(int y, int x, int a[][y]){ //esta função forma um vetor de 2 dimensões
     for (int i=0; i<x; i++){
         for (int j=0; j<y; j++){
-            printf("Indique o valor da entrada (%d, %d):\n", i, j);
+            printf("Indique o valor da entrada (%d, %d):\n", i+1, j+1);
             scanf("%d", &a[i][j]);
         }
     }
@@ -18,5 +29,5 @@ int formarVectorDim2(int y, int x, int a[][y]){ //esta função forma um vetor d
 int random1Dim(int a[], int x){ //esta função devolve um valor random do arry
     time_t t;
     srand((unsigned)time(&t));
-    printf("%d\n", a[rand()%x]);
+    return a[rand()%x];
 }
