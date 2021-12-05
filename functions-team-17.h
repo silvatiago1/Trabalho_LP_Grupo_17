ef99c6724c41f94ef0512de8e0c1005508aa5fcc
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-05 20:48:01 +0000

    Update functions-team-17.h
    conforme o .c correspondente

--- functions-team-17.h
+++ functions-team-17.h
@@ -1,3 +1,3 @@
-int formarVectorDim1(int a[], int x);
-int formarVectorDim2(int y, int x, int a[][y]);
+void formarVectorDim1(int a[], int x);
+void formarVectorDim2(int y, int x, int a[][y]);
 int random1Dim(int a[], int x);
