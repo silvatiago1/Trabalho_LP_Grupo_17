e6214aa80aef51cc52f33a6a213c21ad5d8badca
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-05 20:49:10 +0000

    Update main-team-17.c
    o tamanho é fixo

--- main-team-17.c
+++ main-team-17.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include"functions-team-17.h"
 int main(void){
-    int x;
-    printf("Indique o tamanho do vector:\n");
-    scanf("%d", &x);
+    int x=14;
     int a[x];
     formarVectorDim1(a, x);
     random1Dim(a,x);
+
+
 
     return 0;
 }
