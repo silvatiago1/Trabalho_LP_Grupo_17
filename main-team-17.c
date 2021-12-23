cd7edc23bab3036204fb3686cc2ddefffae412c3
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-23 00:04:03 +0000

    Update main-team-17.c
    Adicionei um pouco do menú ;(

--- main-team-17.c
+++ main-team-17.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 #include"functions-team-17.h"
+#include<stdio.h>
+#include<locale.h>
 int main(void){
     int x=14;
     int a[x];
-    formarVectorDim1(a, x);
-    random1Dim(a,x);
+    int op;
+    formarVectorDim1(a,x);
 
+    printf("1-Retorno de um elemento aleatório desse vector\n2-Cálculo do logaritmo natural de todos os elementos do vector\n3-Devolução do vetor com a primeira metade dos valores ordenada por ordem crescente\n4-Valores do vector que não sáo divisíveis por 3\n5-Média de todos os elementos do vector\n6-...\n");
+    scanf("%d", &op);
+    switch (op)
+    {
+    case 1:
+        printf("O elemento aleatório é:\t %d\n",random1Dim(a,14));
+        break;
+    case 2:
+        ln(a, 14);
+        break;
+    case 3:
+        task3(a, 14);
+        print1dim(a,14);
+        break;
+    
+    default:
+        break;
+    }
 
 
     return 0;
