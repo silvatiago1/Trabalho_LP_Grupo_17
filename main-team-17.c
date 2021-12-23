10e3d89020d9b8852a6ce824a28d17d549f573ba
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-23 01:09:22 +0000

    Update main-team-17.c
    adicionei --help mas sem as coisas

--- main-team-17.c
+++ main-team-17.c
@@ -2,10 +2,14 @@
 #include"functions-team-17.h"
 #include<stdio.h>
 #include<locale.h>
-int main(void){
+#include<string.h>
+int main(int argc,char *argv[]){
     int x=14;
     int a[x];
     int op;
+    if(argc == 2 && strcmp(argv[1], "--help")==0){
+        printf("Ajuda será disponibilizada em breve\n");
+    }
     formarVectorDim1(a,x);
 
     printf("1-Retorno de um elemento aleatório desse vector\n2-Cálculo do logaritmo natural de todos os elementos do vector\n3-Devolução do vetor com a primeira metade dos valores ordenada por ordem crescente\n4-Valores do vector que não sáo divisíveis por 3\n5-Média de todos os elementos do vector\n6-...\n");
