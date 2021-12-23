f26c690d3aea2d33e2c13543a7c5ed56e99e8af3
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-23 00:03:15 +0000

    Update functions-team-17.h
    Adicionei as assinaturas das funções do file .c

--- functions-team-17.h
+++ functions-team-17.h
@@ -1,3 +1,8 @@
 void formarVectorDim1(int a[], int x);
 void formarVectorDim2(int y, int x, int a[][y]);
 int random1Dim(int a[], int x);
+void ln(int a[], int x);
+void print1dim(int a[], int x);
+void task3(int a[], int x);
+int min(int a[], int x, int y, int z);
+int find(int a[], int x, int y, int z, int w);
