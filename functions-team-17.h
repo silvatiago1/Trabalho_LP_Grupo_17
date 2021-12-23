b02740d82b52d9df508ce6ab1c74002115d014a8
ArturPutyato <95541283+ArturPutyato@users.noreply.github.com>
2021-12-23 16:43:58 +0000

    Update functions-team-17.h

--- functions-team-17.h
+++ functions-team-17.h
@@ -1,10 +1,8 @@
- void formarVectorDim1(int a[], int x); //suplementar para formar arrays de 1 dimensão
+void formarVectorDim1(int a[], int x); //suplementar para formar arrays de 1 dimensão
 void formarVectorDim2(int y, int x, int a[][y]); //suplementar para formar arrays de 2 dimensões
 int random1Dim(int a[], int x); //tbm serve para as permutações e para a opção de retornar um valor random 
-void ln(int a[], int x); //pode-se apagar
 void print1dim(int a[], int x); //suplementar para imprimir arrays de uma dimensão
-void task3(int a[], int x);//pode-se apagar 
 int min(int a[], int x, int y, int z); //suplementar para a task 3
 int find(int a[], int x, int y, int z, int w); //suplementar para encontrar o primeiro elemento igual a w
-void task4(int a[], int x); //pode-se apagar
-float averageArray(int a[], int x); //pode-se apagar
+
+
