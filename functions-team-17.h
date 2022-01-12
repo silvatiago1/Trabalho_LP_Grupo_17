03844d9a7c3e48e53b68a1e0db0d9b7afa39fc05
Gonçalo Costa <94967933+Costa-atsoC@users.noreply.github.com>
2022-01-12 23:18:02 +0000

    Update functions-team-17.h

--- functions-team-17.h
+++ functions-team-17.h
@@ -8,7 +8,7 @@ void task3(int a[], int x); //Artur
 float averagearray(int v[],int x);//Exercício 5 da primeira metade
 void imparemprimos(int v[]);
 void permutacao(int a[]); //Exe 6 da 1ªparte
-int newArray(int n[]); // Exe 1 da 2ª parte
+int newArray(int x); // Exe 1 da 2ª parte
 int det(int x, int y, int B[x][y]); //calcula o determinante de qualquer matriz quadrada
 void duplicarMatriz(int x, int y, int a[x][y], int b[x][y]); //clona uma matriz
 void print2(int x, int y, int a[x][y]); //imprime uma matriz
